66ef2ac41ecbaa53564f8fc36295d700f808801b
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-12-07 23:29:59 +1300

    Update dev env libs setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
-var webpack = require( 'webpack' );
-var path = require( 'path' );
+const webpack = require( 'webpack' );
+const path = require( 'path' );
 
 module.exports = {
   devtool: 'eval',
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
-    './index'
+    './index',
   ],
   output: {
     path: path.join( __dirname, 'dist' ),
     filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: '/',
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
   ],
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: [ 'babel' ],
-      include: path.join( __dirname )
-    }]
-  }
-}
+      loaders: ['babel'],
+      include: path.join( __dirname ),
+    }],
+  },
+};
